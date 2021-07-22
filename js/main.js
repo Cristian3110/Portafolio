90f8acac0c08275301c161ec3c3ac4d45849eaa1
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-07-22 00:17:43 -0400

    cambio de texto en el boton darkmode

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@
 function cambio() {
 	const sol = document.getElementById('sol');
 	const luna = document.getElementById('luna');
+	let palabras = document.getElementById('letras');
 
 	if (sol.style.display === 'inline' || luna.style.display === 'none') {
 		luna.style.display = 'inline';
 		sol.style.display = 'none';
+		palabras.innerHTML = 'Light Mode';
 	} else {
 		sol.style.display = 'inline';
 		luna.style.display = 'none';
+		palabras.innerHTML = 'Dark Mode';
 	}
 }
