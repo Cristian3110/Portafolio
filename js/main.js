2a140765ff23cb6985900c88281828b31aa7a029
Cristian3110 <cristiancarrillo311087@gmail.com>
2021-11-04 00:22:08 -0400

    cambios en button darkmode

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ function cambio() {
 	const luna = document.getElementById('luna');
 	let palabras = document.getElementById('letras');
 
-	if (sol.style.display === 'inline' || luna.style.display === 'none') {
-		luna.style.display = 'inline';
-		sol.style.display = 'none';
+	if (sol.style.display === 'none' || luna.style.display === 'inline') {
+		luna.style.display = 'none';
+		sol.style.display = 'inline';
 		palabras.innerHTML = 'Light Mode';
 	} else {
-		sol.style.display = 'inline';
-		luna.style.display = 'none';
+		luna.style.display = 'inline';
+		sol.style.display = 'none';
 		palabras.innerHTML = 'Dark Mode';
 	}
 }
