fe75ae7e50ba3a27f53744f97f7abdb64d456c34
Cristian3110 <cristiancarrillo311087@gmail.com>
2021-12-09 21:13:55 -0400

    configuracion del scroll con animacion navbar completa

--- js/main.js
+++ js/main.js
@@ -21,3 +21,65 @@ function cambio() {
 // scroll navbar
 
 //seleccion activada
+
+const sections = document.querySelectorAll('section');
+const header = document.querySelectorAll('header');
+const footer = document.querySelectorAll('footer');
+
+const options = {
+	threshold: 0.6,
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+	entries.forEach((entry) => {
+		const idItem = entry.target.id;
+		const enlaces = document.querySelectorAll('a');
+
+		if (entry.isIntersecting & (idItem === 'footer')) {
+			enlaces[4].classList.remove('active');
+		}
+		if (entry.isIntersecting & (idItem === 'home')) {
+			enlaces[0].classList.add('active');
+			enlaces[1].classList.remove('active');
+		}
+		if (entry.isIntersecting & (idItem === 'about')) {
+			enlaces[0].classList.remove('active');
+			enlaces[2].classList.remove('active');
+			enlaces[1].classList.add('active');
+		}
+		if (entry.isIntersecting & (idItem === 'skill')) {
+			enlaces[1].classList.remove('active');
+			enlaces[3].classList.remove('active');
+			enlaces[2].classList.add('active');
+		}
+		if (entry.isIntersecting & (idItem === 'project')) {
+			enlaces[2].classList.remove('active');
+			enlaces[4].classList.remove('active');
+			enlaces[3].classList.add('active');
+		}
+		if (entry.isIntersecting & (idItem === 'contact')) {
+			enlaces[3].classList.remove('active');
+			enlaces[4].classList.add('active');
+			footer[0].classList.remove('active');
+		}
+
+		console.log(
+			`Está interceptando la sección ${(entry.isIntersecting, idItem)}`
+		);
+		// console.log(enlaces);
+	});
+}
+
+sections.forEach((section) => {
+	observer.observe(section);
+});
+
+header.forEach((header) => {
+	observer.observe(header);
+});
+
+footer.forEach((footer) => {
+	observer.observe(footer);
+});
